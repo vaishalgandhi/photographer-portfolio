ede3201844e8f30bc0f9570cd603a1724a771b67
Vaishal Gandhi <vaishal48@gmail.com>
2021-02-12 22:43:54 -0500

    Initial setup of laravel lumen api framework

---
+++ api/database/migrations/2021_02_11_212945_create_photographers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotographersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photographers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->string('phone', 15);
+            $table->string('email', 120);
+            $table->string('bio', 200);
+            $table->string('profile_picture');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photographers');
+    }
+}
